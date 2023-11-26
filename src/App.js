be70e2a85783b7b78deca1945c48b265bd3848f3
YoungSTAAR <chinusohlot@gmail.com>
2023-11-26 19:58:04 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Button} from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+        <h1>E-Comm Project</h1>
+        <button>Normal Button</button>
+        <Button>Normal Button</Button>
+    </div>
+  );
+}
+
+export default App;
